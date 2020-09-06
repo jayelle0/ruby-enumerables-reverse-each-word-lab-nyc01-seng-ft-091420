37dac3ef73f4fb5a7a6ec9988e155b80df1672c8
Jacqueline <lojacqueline2@gmail.com>
2020-09-06 01:54:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word (string)
  string = string.split(" ")
   reverse_string = []
   string.each do |element|
-  puts element.reverse 
+reverse_string = element.reverse 
   end 
   reverse_string.join(" ")
 end
