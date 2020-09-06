6d893f54aa3de5baaa33d1d2285c09bb1d02207f
Jacqueline <lojacqueline2@gmail.com>
2020-09-06 02:05:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,10 @@
-require "pry"
+
 
 def reverse_each_word (string)
  string = string.split(" ")
   reverse_string = []
   string.each do |element|
 reverse_string = element.reverse
-
-binding.pry 
 
   end 
   reverse_string.join(" ")
